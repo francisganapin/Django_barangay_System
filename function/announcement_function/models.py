8b8c550abc19537a72c0edf0b42dfcd799e61a86
francis Valenzuela Ganapin <66492676+francisganapin@users.noreply.github.com>
2025-02-16 12:54:26 +0800

    2/16/2025

--- function/announcement_function/models.py
+++ function/announcement_function/models.py
@@ -1,2 +1,7 @@
 from django.db import models
 
+class Announcement(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
--- function/permits_function/models.py
+++ function/permits_function/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from resident_function.models import Residents_model
 
-# Create your models here.
+# Create your views here.
+class Permit(models.Model):
+    permit_type = models.Charfield(max_lenght=100)
+    issued_date = models.DateField()
+    expiration_date = models.DateField()
+    status = models.CharField(
+        max_length=20,
+        choices=[('Pending','pending'),('Approve','Approve'),('Rejected','rejected')],
+        default="Pending",
+    )
+    resident = models.ForeignKey(Residents_model,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f'{self.permit_type} - {self.resident.first_name}'
